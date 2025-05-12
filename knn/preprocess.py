8bed4254289bdb5eac94511a112eb5b4b4d79337
Paisal Tanjung <paisaltanjung1102@gmail.com>
2025-05-12 11:50:52 +0700

    fix preprocessing & compute

--- knn/preprocess.py
+++ knn/preprocess.py
@@ -7,14 +7,13 @@ df = pd.read_csv("diabetes.csv")
 features = df.drop(columns=["Outcome"])
 
 # Pisahkan berdasarkan label diabetes
-diabetic = df[df["Outcome"] == 1].drop(columns=["Outcome"])  # Data pasien diabetes
-non_diabetic = df[df["Outcome"] == 0].drop(columns=["Outcome"])  # Data pasien non-diabetes
+diabetic = df[df["Outcome"] == 1].drop(columns=["Outcome"])  #diabetes
+non_diabetic = df[df["Outcome"] == 0].drop(columns=["Outcome"])  #non-diabetes
 
-# Hitung rata-rata tiap fitur untuk masing-masing kelompok
+# Hitung rata-rata tiap fitur untuk masing-masing
 diabetic_avg = diabetic.mean(numeric_only=True).to_dict()
 non_diabetic_avg = non_diabetic.mean(numeric_only=True).to_dict()
 
-# Simpan hasil ke dalam file CSV
 df_avg = pd.DataFrame([diabetic_avg, non_diabetic_avg], index=["Diabetic", "Non-Diabetic"])
 df_avg.to_csv("preprocessed_data.csv")
 
